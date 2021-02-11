857881f13baebc0f5e8b88b8fc30f3f8b7fbfa2d
RobertGengiu <36796757+RobertGengiu@users.noreply.github.com>
2021-02-12 00:00:30 +0200

    Add files via upload

---
+++ Hit.hlsl
@@ -0,0 +1,34 @@
+#include "Common.hlsl"
+
+// #DXR Extra: Per-Instance Data
+// #DXR Extra: Per-Instance Data
+cbuffer Colors : register(b0)
+{
+    float3 A;
+    float3 B;
+    float3 C;
+}
+
+[shader("closesthit")] 
+void ClosestHit(inout HitInfo payload, Attributes attrib) 
+{
+    float3 barycentrics =
+        float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
+
+    float3 hitColor = A * barycentrics.x + B * barycentrics.y + C * barycentrics.z;
+
+    payload.colorAndDistance = float4(hitColor, RayTCurrent());
+}
+
+// #DXR Extra: Per-Instance Data
+[shader("closesthit")]
+void PlaneClosestHit(inout HitInfo payload, Attributes attrib)
+{
+    float3 barycentrics =
+        float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
+
+    float3 hitColor = float3(0.7 * barycentrics.x, 0.7 * barycentrics.x, 0.3 * barycentrics.x);
+
+    payload.colorAndDistance = float4(hitColor, RayTCurrent());
+}
+
